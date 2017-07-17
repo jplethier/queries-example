49ce3b686fb23e8001609fc8aaf68801df2fa2cd
João Paulo Lethier <jplethier@gmail.com>
2017-07-16 21:01:10 -0300

    adds seeds and example scopes;

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,55 @@
 class Book < ApplicationRecord
+  scope :ar_category, -> (category) { where(category: category) }
+  scope :arel_category, -> (category) { where(arel_table[:category].eq(category)) }
+
+  scope :ar_category_or_title, -> (category, title) { where('category = ? or title = ?', category, title) }
+  scope :arel_category_or_title, -> (category, title) { where(arel_table[:category].eq(category).or(arel_table[:title].eq(title))) }
+
+  # Book.ar_category_or_title_like_or_description_like('drama', 'Harry Potter').to_sql
+  # => "SELECT \"books\".* FROM \"books\"
+  #       WHERE (category = 'drama' or title like '%Harry Potter%' or description like '%Harry Potter%')"
+  scope :ar_category_or_title_like_or_description_like, lambda{ |category, partial_search|
+    where("category = ? or title like ? or description like ?", category, '%' + partial_search + '%', '%' + partial_search + '%')
+  }
+  # Book.arel_category_or_title_like_or_description_like('drama', 'Harry Potter').to_sql
+  # => "SELECT \"books\".* FROM \"books\"
+  #       WHERE (\"books\".\"category\" = 'drama' OR (\"books\".\"title\" LIKE 'Harry Potter' OR \"books\".\"description\" LIKE 'Harry Potter'))"
+  scope :arel_category_or_title_like_or_description_like, lambda{ |category, partial_search|
+    where(arel_table[:category].eq(category)
+      .or(arel_table[:title].matches(partial_search)
+      .or(arel_table[:description].matches(partial_search))))
+  }
+
+  # Book.ar_published_between(3.years.ago, 1.year.ago).to_sql
+  # => "SELECT \"books\".* FROM \"books\"
+  #       WHERE (\"books\".\"published_at\" BETWEEN '2014-07-16 23:19:13.327294' AND '2016-07-16 23:19:13.327789')"
+  scope :ar_published_between, -> (bg_date, end_date) { where(published_at: bg_date..end_date) }
+
+  # Book.ar_less_pages_than(10).to_sql
+  # => "SELECT \"books\".* FROM \"books\" WHERE (pages_count < 10)"
+  scope :ar_less_pages_than, -> (pg_count) { where('pages_count < ?', pg_count) }
+  # Book.arel_less_pages_than(10).to_sql
+  #  => "SELECT \"books\".* FROM \"books\" WHERE (\"books\".\"pages_count\" < 10)"
+  scope :arel_less_pages_than, -> (pg_count) { where(arel_table[:pages_count].lt(pg_count)) }
+
+  # Book.ar_more_pages_than(10).to_sql
+  # => "SELECT \"books\".* FROM \"books\" WHERE (pages_count > 10)"
+  scope :ar_more_pages_than, -> (pg_count) { where('pages_count > ?', pg_count) }
+  # Book.arel_more_pages_than(10).to_sql
+  #  => "SELECT \"books\".* FROM \"books\" WHERE (\"books\".\"pages_count\" > 10)"
+  scope :arel_more_pages_than, -> (pg_count) { where(arel_table[:pages_count].gt(pg_count)) }
+
+  # Book.ar_more_or_equal_pages(10).to_sql
+  # => "SELECT \"books\".* FROM \"books\" WHERE (pages_count >= 10)"
+  scope :ar_more_or_equal_pages, -> (pg_count) { where('pages_count >= ?', pg_count) }
+  # Book.arel_more_or_equal_pages(10).to_sql
+  #  => "SELECT \"books\".* FROM \"books\" WHERE (\"books\".\"pages_count\" >= 10)"
+  scope :arel_more_or_equal_pages, -> (pg_count) { where(arel_table[:pages_count].gteq(pg_count)) }
+
+  # Book.ar_less_or_equal_pages(10).to_sql
+  # => "SELECT \"books\".* FROM \"books\" WHERE (pages_count <= 10)"
+  scope :ar_less_or_equal_pages, -> (pg_count) { where('pages_count <= ?', pg_count) }
+  # Book.arel_less_or_equal_pages(10).to_sql
+  #  => "SELECT \"books\".* FROM \"books\" WHERE (\"books\".\"pages_count\" <= 10)"
+  scope :arel_less_or_equal_pages, -> (pg_count) { where(arel_table[:pages_count].lteq(pg_count)) }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Publisher.create(name: 'Rocco', country: 'Brazil')
+Publisher.create(name: 'Pearson', country: 'United Kingdom')
+Publisher.create(name: 'Holtzbrinck', country: 'Germany')
+Publisher.create(name: 'Oxford University Press', country: 'United Kingdom')
+Publisher.create(name: 'Simon & Schuster', country: 'United States')
+Publisher.create(name: 'Mondadori', country: 'Italy')
+Publisher.create(name: 'Saraiva', country: 'Brazil')
+Publisher.create(name: 'Atlas', country: 'Brazil')
+harper_collins = Publisher.create(name: 'HarperCollins', country: 'United States')
+presenca = Publisher.create(name: 'Presenca', country: 'Brazil')
+
+Author.create(first_name: 'Joanne', last_name: 'Rowling', birthday: '1965-07-31', country: 'United Kingdom')
+Author.create(first_name: 'Arthur', last_name: 'Doyle', birthday: '1930-07-07', country: 'United Kingdom')
+Author.create(first_name: 'Agatha', last_name: 'Christie', birthday: '1890-09-15', country: 'United Kingdom')
+c_s_lewis = Author.create(first_name: 'Clive', last_name: 'Lewis', birthday: '1898-11-22', country: 'United Kingdom')
+Author.create(first_name: 'George', last_name: 'Martin', birthday: '1948-09-20', country: 'United States')
+jo_soares = Author.create(first_name: 'Jo', last_name: 'Soares', birthday: '1938-01-16', country: 'Brazil')
+
+Book.create(title: 'The Chronicles of Narnia - The Magician\'s Nephew', author: c_s_lewis, publisher: harper_collins, description: 'Witness the creation of a magical land in The Magician's Nephew, the first title in C. S. Lewis's classic fantasy series, which has captivated readers of all ages for over sixty years. This beautiful hardcover edition features jacket art by three-time Caldecott Medal-winning illustrator David Wiesner and black-and-white interior art by the series', published_at: '1996-01-01', average_rating: 4.8, pages_count: 522, category: 'fantasy')
+Book.create(title: 'O Xango de Baker Street', author: jo_soares, publisher: presenca, description: 'A diva francesa Sarah Bernhardt pela primeira vez se apresenta no Brasil. O público se curva perante o talento de Sarah, incluindo o imperador Dom Pedro II, que lhe conta um segredo: um valioso violino Stradivarius, um presente seu à baronesa Maria Luíza, desaparecera misteriosamente.', published_at: '1996-01-01', average_rating: 4.1, pages_count: 325, category: 'drama')
